19aa32491d70d95b4df90bf4c20a17a4e8aec3d0
bpancost <brandon@pancost.org>
2020-08-18 13:47:16 -0700

    fixed logging wrapping args in slices

--- internal/logging/logrus_logger.go
+++ internal/logging/logrus_logger.go
@@ -35,101 +35,101 @@ func GetLoggerWithBaseFields(projectName string, hostName string) Logger {
 }
 
 func Trace(args ...interface{}) {
-	log.Trace(args)
+	log.Trace(args...)
 }
 func (log *LogrusLogger) Trace(args ...interface{}) {
-	log.logger.Trace(args)
+	log.logger.Trace(args...)
 }
 
 func Tracef(format string, args ...interface{}) {
-	log.Tracef(format, args)
+	log.Tracef(format, args...)
 }
 func (log *LogrusLogger) Tracef(format string, args ...interface{}) {
-	log.logger.Tracef(format, args)
+	log.logger.Tracef(format, args...)
 }
 
 func Debug(args ...interface{}) {
-	log.Debug(args)
+	log.Debug(args...)
 }
 func (log *LogrusLogger) Debug(args ...interface{}) {
-	log.logger.Debug(args)
+	log.logger.Debug(args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	log.Debugf(format, args)
+	log.Debugf(format, args...)
 }
 func (log *LogrusLogger) Debugf(format string, args ...interface{}) {
-	log.logger.Debugf(format, args)
+	log.logger.Debugf(format, args...)
 }
 
 func Info(args ...interface{}) {
-	log.Info(args)
+	log.Info(args...)
 }
 func (log *LogrusLogger) Info(args ...interface{}) {
-	log.logger.Info(args)
+	log.logger.Info(args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	log.Infof(format, args)
+	log.Infof(format, args...)
 }
 func (log *LogrusLogger) Infof(format string, args ...interface{}) {
-	log.logger.Infof(format, args)
+	log.logger.Infof(format, args...)
 }
 
 func Warn(args ...interface{}) {
-	log.Warn(args)
+	log.Warn(args...)
 }
 func (log *LogrusLogger) Warn(args ...interface{}) {
-	log.logger.Warn(args)
+	log.logger.Warn(args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	log.Warnf(format, args)
+	log.Warnf(format, args...)
 }
 func (log *LogrusLogger) Warnf(format string, args ...interface{}) {
-	log.logger.Warnf(format, args)
+	log.logger.Warnf(format, args...)
 }
 
 func Error(args ...interface{}) {
-	log.Error(args)
+	log.Error(args...)
 }
 func (log *LogrusLogger) Error(args ...interface{}) {
-	log.logger.Error(args)
+	log.logger.Error(args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	log.Errorf(format, args)
+	log.Errorf(format, args...)
 }
 func (log *LogrusLogger) Errorf(format string, args ...interface{}) {
-	log.logger.Errorf(format, args)
+	log.logger.Errorf(format, args...)
 }
 
 func Fatal(args ...interface{}) {
-	log.Fatal(args)
+	log.Fatal(args...)
 }
 func (log *LogrusLogger) Fatal(args ...interface{}) {
-	log.logger.Fatal(args)
+	log.logger.Fatal(args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	log.Fatalf(format, args)
+	log.Fatalf(format, args...)
 }
 func (log *LogrusLogger) Fatalf(format string, args ...interface{}) {
-	log.logger.Fatalf(format, args)
+	log.logger.Fatalf(format, args...)
 }
 
 func Panic(args ...interface{}) {
-	log.Panic(args)
+	log.Panic(args...)
 }
 func (log *LogrusLogger) Panic(args ...interface{}) {
-	log.logger.Panic(args)
+	log.logger.Panic(args...)
 }
 
 func Panicf(format string, args ...interface{}) {
-	log.Panicf(format, args)
+	log.Panicf(format, args...)
 }
 func (log *LogrusLogger) Panicf(format string, args ...interface{}) {
-	log.logger.Panicf(format, args)
+	log.logger.Panicf(format, args...)
 }
 
 func AddRestRequestLogger(req *http.Request) *http.Request {
